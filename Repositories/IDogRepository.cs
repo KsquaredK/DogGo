d48ddb866360a57f2cb6364428d77fa45b41d707
Kelly Kessler <ksquaredkelly@gmail.com>
2021-09-23 09:00:06 -0500

    WIP OWnerViewModel implementation

--- Repositories/IDogRepository.cs
+++ Repositories/IDogRepository.cs
@@ -9,6 +9,8 @@ namespace DogGo.Repositories
 
         Dog GetDogById(int id);
 
+        List<Dog> GetDogsByOwnerId(int ownerId);
+
         void AddDog(Dog dog);
 
         void UpdateDog(Dog dog);

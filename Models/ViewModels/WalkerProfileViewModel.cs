bed92ddbf6642c18db1d023cf567db375d7414e7
Kelly Kessler <ksquaredkelly@gmail.com>
2021-09-23 16:24:30 -0500

    WIP walks in walker profile

---
+++ Models/ViewModels/WalkerProfileViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Models.ViewModels
+{
+    public class WalkerProfileViewModel
+    {
+        public Walker Walker { get; set; }
+
+        public Neighborhood Neighborhood { get; set; }
+
+        public List<Owner> Owners { get; set; }
+
+        public List<Walk> Walks { get; set; }
+    }
+}
+
+
+//join walker to owner by walk.dogId > dog.ownerId > owner.id in repo
---
+++ Models/Walk.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Models.ViewModels
+{
+    public class Walk
+    {
+        public int Id { get; set; }
+        public string Duration { get; set; }
+        public int OwnerId { get; set; }
+        public Neighborhood Neighborhood { get; set; }
+        public Owner Owner { get; set; }
+
+        public Walker WalkerId { get; set; }
+
+        public Dog DogId { get; set; }
+
+        public Dog Dog { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int TotalDuration { get; set; }
+
+
+    }
+}
